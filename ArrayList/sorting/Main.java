c4038cd8b66fb32440523c1dba7a286bf276fe77
Anas Makhloufi <102997226+anasmak04@users.noreply.github.com>
2023-05-09 10:08:47 +0000

    sorting using collections class

---
+++ ArrayList/sorting/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        List <Integer> list = new ArrayList <> ();
+        
+        list.add(1);
+        list.add(5);
+        list.add(70);
+        list.add(8);
+        
+        Collections.sort(list);
+        
+        System.out.println(list);
+
+        
+        
+        
+        
+    }
+}
